e420efb314b70ffb15a5d4048ba79d92f2eaed02
Raido Oras <oras.raido@gmail.com>
2024-04-28 05:14:53 +0300

    feat: add word and character counting
    16: Print a report
    Add code for counting, sorting and printing alphabet character counts of
    a file

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
+def countCharacters(text):
+    chars = {} 
+    for c in text.lower():
+        if not c.isalpha():
+            continue
+
+        if c in chars:
+            chars[c] += 1
+        else:
+            chars[c] = 1
+
+    structuredCharList = []
+    for k, v in chars.items():
+        structuredCharList.append({"char": k, "count": v})
+
+    structuredCharList.sort(reverse=True, key=lambda d : d["count"])
+    return structuredCharList
+
+def prettyPrintCounts(structuredCharList):
+    for i in structuredCharList:
+        print(f"The '{i["char"]}' character war found {i["count"]} times")
+
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
+
         words = file_contents.split()
-        print(len(words))
+        print(f"{len(words)} words found in the document\n")
+
+        structuredCharList = countCharacters(file_contents)
+        prettyPrintCounts(structuredCharList)
 
 main()
