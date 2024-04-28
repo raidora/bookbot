0597e2c0082bfec50ecfbafb092209ad7f41c84e
Raido Oras <oras.raido@gmail.com>
2024-04-28 04:29:15 +0300

    feat: print word count

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        words = file_contents.split()
+        print(len(words))
 
 main()
